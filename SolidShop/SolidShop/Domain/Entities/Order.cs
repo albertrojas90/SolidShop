2e065bffc8bea5b5a0af394af80897a88256a551
AlbertRojas90 <albert.rojas097@gmail.com>
2025-08-24 13:22:03 -0500

    estructura de orden

--- SolidShop/SolidShop/Domain/Entities/Order.cs
+++ SolidShop/SolidShop/Domain/Entities/Order.cs
@@ -6,7 +6,12 @@ using System.Threading.Tasks;
 
 namespace SolidShop.Domain.Entities
 {
-    internal class Order
+    public class Order
     {
+        public int Id { get; set; }
+        public string CustomerName { get; set; } = string.Empty;
+        public List<OrderLine> Lines { get; } = new();
+
+        public decimal Subtotal => Lines.Sum(l => l.LineTotal);
     }
 }
