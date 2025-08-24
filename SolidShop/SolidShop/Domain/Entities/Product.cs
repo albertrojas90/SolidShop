6791b07d77195956546dcda9382ab4f02aa930cf
AlbertRojas90 <albert.rojas097@gmail.com>
2025-08-24 11:13:19 -0500

    inicio proyecto

---
+++ SolidShop/SolidShop/Domain/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidShop.Domain.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; init; } = string.Empty;
+        public decimal UniPrice {  get; init; }
+
+    }
+}
